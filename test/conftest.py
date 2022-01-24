244c765b3fba6ab86853916cf962a237e84e4311
Jakub Jamro <kuba.jamro@gmail.com>
2022-01-24 00:10:36 +0000

    add account links
    Support income/expenses view #2

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,23 @@
 import os
 import sys
 from pathlib import Path
-from typing import Callable
 
 import bs4
 import jinja2 as jinja2
 import pytest as pytest
+from fava.application import app, _pull_beancount_file
 from fava.core import FavaLedger
-from fava.template_filters import FILTERS
 from pytest import fixture
+
+app.config['BEANCOUNT_FILES'] = [os.path.join(os.path.dirname(__file__), 'example.beancount')]
+
+
+@fixture
+def client():
+    with app.app_context():
+        with app.test_request_context():
+            _pull_beancount_file(None, {'bfile': 'beancount'})
+            yield app
 
 
 @fixture
@@ -23,17 +32,11 @@ def extension_template():
     extension_directory = Path(os.path.dirname(__file__)).parent.absolute()
     fava_directory = os.path.dirname(sys.modules['fava'].__file__)
 
-    def add_template_filters(env: jinja2.Environment, filters: list[Callable]) -> jinja2.Environment:
-        for f in filters:
-            env.filters[f.__name__] = f
-        return env
-
     def template_loader(template_file):
-        join = os.path.join(fava_directory, 'templates')
-        loader = jinja2.FileSystemLoader(searchpath=[os.path.join(extension_directory, 'fava_review', 'templates'),
-                                                     join])
-        env = add_template_filters(jinja2.Environment(loader=loader), FILTERS)
-        return env.get_template(template_file)
+        app.jinja_env.loader = jinja2.FileSystemLoader(
+            searchpath=[os.path.join(extension_directory, 'fava_review', 'templates'),
+                        (os.path.join(fava_directory, 'templates'))])
+        return app.jinja_env.get_template(template_file)
 
     return template_loader
 
--- test/test_income_expense_report_template.py
+++ test/test_income_expense_report_template.py
@@ -7,18 +7,21 @@ from hamcrest.core.matcher import Matcher
 from fava_review import FavaIncomeExpenseReview
 
 
-def test_table_header_contains_all_fields(example_ledger: FavaLedger, extension_template_soup: callable) -> None:
-    template_soup = extension_template_soup("FavaIncomeExpenseReview.html", FavaIncomeExpenseReview(example_ledger))
-    tags: Sequence[str] = [t.get_text() for t in template_soup.find('thead').find_all('th')]
+def test_table_header_contains_all_fields(example_ledger: FavaLedger, extension_template_soup: callable,
+                                          client) -> None:
+    template_soup = extension_template_soup("FavaIncomeExpenseReview.html",
+                                            FavaIncomeExpenseReview(example_ledger))
+    tags: Sequence[str] = [t.get_text().strip() for t in template_soup.select('thead th')]
     matchers: Matcher[Sequence[str]] = contains_exactly(
         'account', '2020-10', '2020-11', '2020-12', '2021-01', '2021-02', '2021-03', '2021-04', 'total')
     assert_that(tags, matchers)
 
 
-def test_table_body_contains_all_fields(example_ledger: FavaLedger, extension_template_soup: callable) -> None:
+def test_table_body_contains_all_fields(example_ledger: FavaLedger, extension_template_soup: callable,
+                                        client) -> None:
     template_soup = extension_template_soup("FavaIncomeExpenseReview.html", FavaIncomeExpenseReview(example_ledger))
 
-    tags: Sequence[Sequence[str]] = [[c.get_text() for c in row.find_all('td')]
+    tags: Sequence[Sequence[str]] = [[c.get_text().strip() for c in row.find_all('td')]
                                      for row in template_soup.select('tbody tr')]
     matchers: Matcher[Sequence[Sequence[str]]] = contains_exactly(
         ['Expenses:Groceries', '10.00', '20.00', '30.00', '0', '0', '60.00', '70.00', '190.00'],
@@ -27,9 +30,19 @@ def test_table_body_contains_all_fields(example_ledger: FavaLedger, extension_te
     assert_that(tags, matchers)
 
 
-def test_table_footer_contains_all_fields(example_ledger: FavaLedger, extension_template_soup: callable) -> None:
+def test_table_footer_contains_all_fields(example_ledger: FavaLedger, extension_template_soup: callable,
+                                          client) -> None:
     template_soup = extension_template_soup("FavaIncomeExpenseReview.html", FavaIncomeExpenseReview(example_ledger))
-    tags: Sequence[str] = [t.get_text() for t in template_soup.find('tfoot').find_all('td')]
+    tags: Sequence[str] = [t.get_text() for t in template_soup.select('tfoot td')]
     matchers: Matcher[Sequence[str]] = contains_exactly(
         'total', '-990.00', '-980.00', '-970.00', '-1000.00', '-1000.00', '-940.00', '-930.00', '-6810.00')
     assert_that(tags, matchers)
+
+
+def test_table_body_account_names_are_links(example_ledger: FavaLedger, extension_template_soup: callable,
+                                            client) -> None:
+    template_soup = extension_template_soup("FavaIncomeExpenseReview.html", FavaIncomeExpenseReview(example_ledger))
+    tags: Sequence[str] = [link.get_text().strip() for link in template_soup.select('tbody td a')]
+    matchers: Matcher[Sequence[str]] = contains_exactly('Expenses:Groceries', 'Income:Salary:ABC')
+    assert_that(tags, matchers)
+
